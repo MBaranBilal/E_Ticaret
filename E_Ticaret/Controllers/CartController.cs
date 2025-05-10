2f010bd0991e4b9bd388027ccbc30322f45c34e8
M.Baran Bilal <mahfuzbaranbilal@gmail.com>
2025-05-10 22:52:32 +0300

    Alışveriş kısmı (ürünlerin listelenmesi) tamamlandı.

--- E_Ticaret/Controllers/CartController.cs
+++ E_Ticaret/Controllers/CartController.cs
@@ -1,12 +1,76 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Ticaret.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_Ticaret.Controllers
 {
     public class CartController : Controller
     {
+        private readonly ETicaretDbContext context;
+
+        public CartController(ETicaretDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
         public IActionResult Cart()
         {
-            return View();
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if(userId == null)//yani kullanıcı girişi yapılmamışsa
+            {
+                return RedirectToAction("Login", "User");
+            }
+
+            var items=context.Cards.Include( c=> c.Product).Where(c=> c.UserId == userId).ToList();
+            // card ve product tablolarımız ilişkili olduğu için product bilgilerine de ulaşabiliriz. Bunun için include fonksiyonuyla aslında SQL tarafında JOIN işlemi yaptığımızı düşünebiliriz.
+            //where sorgusu zaten bildğimiz gibi, sadece session ile aldığımız userid ile eşleşen kullanıcının sepeti gözüksün istiyoruz.
+            //önyüze liste halinde göndermek için .tolist metodunu ekliyoruz sona
+            return View(items);
         }
-    }
+
+        public IActionResult AddToCart(int id)// tıkladığımız ürünü id ile saptıyoruz
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if (userId == null)
+            {
+                return RedirectToAction("Login", "User");
+            }
+                
+
+            var varOlanUrun = context.Cards.FirstOrDefault(c => c.UserId == userId && c.ProductId == id);//ürün önceden eklenmişse burada buluyoruz
+            if (varOlanUrun != null)//ürün önceden eklenmişse sayısını 1 arttırıyoruz
+            {
+                varOlanUrun.Quantity += 1;
+            }
+            else//ürün daha önce eklenmemişse bilgileriyle eklioruz
+            {
+                context.Cards.Add(new Card
+                {
+                    UserId = userId.Value,
+                    ProductId = id,
+                    Quantity = 1
+                });
+            }
+
+            context.SaveChanges();
+            return RedirectToAction("Cart");
+        }
+
+		public IActionResult Delete(int id)
+		{
+			var userId = HttpContext.Session.GetInt32("UserId");
+			if (userId == null) return RedirectToAction("Login", "User");
+
+			var item = context.Cards.FirstOrDefault(c => c.CardId == id && c.UserId == userId);
+			if (item != null)
+			{
+				context.Cards.Remove(item);
+				context.SaveChanges();
+			}
+
+			return RedirectToAction("Cart");
+		}
+
+	}
 }
