692b1d6e3e317ddf057ba66f096962b862aa508a
AhmetSOGUT <01.ahmet.sgt@gmail.com>
2025-05-10 10:02:17 +0300

    Admin Page

---
+++ E_Ticaret/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using E_Ticaret.Models;
+
+public class AdminController : Controller
+{
+    private readonly ETicaretDbContext _context;
+
+    public AdminController(ETicaretDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Users()
+    {
+        var users = _context.Users.ToList();
+        return View(users); // Users.cshtml'ye gönder
+    }
+}
